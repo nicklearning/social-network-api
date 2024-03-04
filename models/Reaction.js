ba63ee4524ba526764fab0a2aad81d5529ff936b
Nick Smith <nick.smith.asu@gmail.com>
2024-03-04 15:26:50 -0700

    Define Reaction and Thought models

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema } = require('mongoose');
+
+// Reaction Schema
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Schema.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
+
+
+reactionSchema.path('createdAt').get(function (createdAt) {
+    return createdAt.toLocaleString();
+});
+
+module.exports = reactionSchema;
