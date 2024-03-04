9579946b712f39757ba93ef5e6a2d4433be63653
Nick Smith <nick.smith.asu@gmail.com>
2024-03-04 16:57:22 -0700

    POST, PUT, DELETE thought routes created

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,4 +9,62 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-}
+
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({
+                _id: req.params.thoughtId
+            })
+                .select('-__v');
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                req.body,
+                { new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+
+            res.json(thought); // Return the updated user
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Server Error' });
+        }
+    },
+
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+            res.json({ message: 'Thought deleted!' })
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const {
-  getThoughts,
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
 } = require('../../controllers/thoughtController.js');
 
-router.route('/').get(getThoughts)
+router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
 
 module.exports = router;
