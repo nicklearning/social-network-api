e830e4be714bc4f45700e21e6923cb327cbad71f
Nick Smith <nick.smith.asu@gmail.com>
2024-03-04 16:25:31 -0700

    Define POST, PUT, and DELETE routes for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User, Thought } = require('../models');
 
 module.exports = {
   async getUsers(req, res) {
@@ -29,6 +29,44 @@ module.exports = {
       res.json(dbUserData);
     } catch (err) {
       res.status(500).json(err)
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        req.body,
+        { new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user); // Return the updated user
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Thought.deleteMany({
+        _id: {
+          $in: user.thoughts
+        }
+      });
+      res.json({ message: 'User and associated thoughts deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
     }
   },
 };
